255e432776ccc9048d8d9e1c942c437b14381825
nataliedutz <natalie.dutz@dci-student.org>
2024-02-14 11:29:27 +0100

    Add comments to project urls file

--- django_project/urls.py
+++ django_project/urls.py
@@ -4,9 +4,18 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    # Admin site URL
     path("admin/", admin.site.urls),
+
+    # Include URLs from the 'posts' app under the 'api/v1/' prefix
     path("api/v1/", include("posts.urls")),
-    path("api-auth", include("rest_framework.urls")),
+
+    # Include default authentication URLs provided by Django REST Framework
+    path("api-auth/", include("rest_framework.urls")),
+
+    # Include URLs for authentication provided by dj-rest-auth app
     path("api/v1/dj-rest-auth/", include("dj_rest_auth.urls")),
-    path("api/v1/dj-rest-auth/registration", include("dj_rest_auth.registration.urls"))
+
+    # Include URLs for registration provided by dj-rest-auth app
+    path("api/v1/dj-rest-auth/registration/", include("dj_rest_auth.registration.urls"))
 ]
